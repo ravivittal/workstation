4c3b22f66d57419dca6c96c64fdaaa15e91270b3
Ravi Vittal <rvittal_dba@yahoo.com>
2017-09-12 08:47:15 +0000

    Commit

---
+++ recipes/setup.rb
@@ -0,0 +1,18 @@
+package %w(tree ntp) do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+file '/etc/motd' do
+  content 'This Server is Ravi Vittal`s Property'
+  mode '0700'
+  owner 'root'
+  group 'wheel'
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
