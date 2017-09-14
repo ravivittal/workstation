e402d1179da795452625c27a7700ac128b42aded
ravivittal <rvittal_dba@yahoo.com>
2017-09-14 07:34:10 +0000

    New Recipe with Template

--- recipes/setup.rb
+++ recipes/setup.rb
@@ -6,11 +6,9 @@ package 'git' do
   action :install
 end
 
-file '/etc/motd' do
-  content 'This Server is Ravi Vittal`s Property'
-  mode '0700'
-  owner 'root'
-  group 'wheel'
+template '/etc/motd' do
+  source 'motd.erb'
+  action :create
 end
 
 service 'ntpd' do
